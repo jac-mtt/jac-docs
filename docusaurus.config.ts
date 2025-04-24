8b14a83fa84ee411823b7aac8bcd76897943419a
benyamins <benjarq@hotmail.com>
2025-04-24 16:53:30 -0400

    reuso de datos

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,8 +33,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: 'es',
+    locales: ['es'],
   },
   markdown: {
     mermaid: true
@@ -143,6 +143,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.nightOwl,
+      additionalLanguages: ['r', 'bash'],
     },
   } satisfies Preset.ThemeConfig,
 };
